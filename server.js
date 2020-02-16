e543aaeb63d7e9593fc8eca95cc4f84aee85795d
Razzi Abuissa <razzi53@gmail.com>
2020-02-15 16:20:02 -0800

    Add /threats endpoint, reading from mongodb

--- server.js
+++ server.js
@@ -3,16 +3,31 @@ const express = require('express');
 const path = require('path');
 const port = process.env.EXPRESS_PORT || 4000;
 
+const MongoClient = require('mongodb').MongoClient
+const mongoUrl = 'mongodb://localhost:27017'
+const dbName = 'evacuate'
+
+let db;
+
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, './client/public')))
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/more', (req, res) => {
-  res.send('hey');
+app.get('/threats', async (req, res) => {
+  const collection = db.collection('threats')
+
+  const threats = await collection.find().toArray()
+
+  res.send({ threats });
 })
 app.get('/use', (req, res) => {
   res.send('great');
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+MongoClient.connect(mongoUrl, (err, client) => {
+  if (err) return console.log(err);
+
+  db = client.db(dbName)
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+});
