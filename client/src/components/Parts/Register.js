b7d364fd0c3d46bb19f5382673baa27081ec0a29
Jiani Fan <jiani.fan@gmail.com>
2020-02-16 12:09:40 -0800

    Style topbar

--- client/src/components/Parts/Register.js
+++ client/src/components/Parts/Register.js
@@ -2,40 +2,53 @@ import React, { useState } from 'react';
 import './register.scss';
 export const Register = () => {
   const [user, setUser] = useState(null);
+  const username = user ? `User: ${ user }` : '';
 
-  return (
-    <div className="register">
-      User: {user}
-      <div
-        onClick={() => {
-          gapi.auth2
-            .getAuthInstance()
-            .signIn()
-            .then(() => {
-              gapi.client.people.people
-                .get({
-                  resourceName: 'people/me',
-                  'requestMask.includeField': 'person.names'
-                })
-                .then(function(resp) {
-                  setUser(resp.result.names[0].givenName);
-                });
-            })
-            .catch(err => {
-              console.log(err);
-            });
-        }}
-      >
-        Register now
+  if (user) {
+    return (
+      <div className="register">
+        {username}
+
+        <button
+          onClick={() => {
+            gapi.auth2.getAuthInstance().signOut();
+            setUser(null);
+          }}
+        >
+          Logout
+        </button>
+
       </div>
-      <div
-        onClick={() => {
-          gapi.auth2.getAuthInstance().signOut();
-          setUser(null);
-        }}
-      >
-        Sign out
+    )
+  } else {
+    return (
+      <div className="register">
+        {username}
+
+        <button
+          onClick={() => {
+            gapi.auth2
+              .getAuthInstance()
+              .signIn()
+              .then(() => {
+                gapi.client.people.people
+                  .get({
+                    resourceName: 'people/me',
+                    'requestMask.includeField': 'person.names'
+                  })
+                  .then(function (resp) {
+                    setUser(resp.result.names[0].givenName);
+                  });
+              })
+              .catch(err => {
+                console.log(err);
+              });
+          }}
+        >
+          Register Now
+        </button>
+
       </div>
-    </div>
-  );
+    )
+  }
 };
